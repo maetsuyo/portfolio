fda596235fe3e6c803527b633bf523b8147f0131
maetsuyo <maenoorehatuyokatta8009@gmail.com>
2025-10-28 17:09:19 +0900

    HomeとProfile分ける

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white p-4">
-      <div className="text-4xl font-bold">
-        <p>作成途中</p>
-      </div>
+    <footer className="bg-gray-800 text-white p-4 text-3xl flex justify-center space-x-10">
+      <button className="px-6 py-2 rounded transition-colors duration-200 hover:bg-gray-700">
+        Home
+      </button>
+      <button className="px-6 py-2 rounded transition-colors duration-200 hover:bg-gray-700">
+        Profile
+      </button>
     </footer>
   );
 }
